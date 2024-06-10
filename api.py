7be8977f576e5ebe851995693822917e70f639db
Gustavo N. Santiago <104911112+GustavoSantiago113@users.noreply.github.com>
2024-06-10 14:10:04 -0500

    first commit

---
+++ api.py
@@ -0,0 +1,42 @@
+# Importing Libraries
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from flask_cors import CORS
+from werkzeug.serving import WSGIRequestHandler
+
+# Creating the application
+app = Flask(__name__)
+CORS(app)
+
+# Information of the mongoDB database
+client = MongoClient('******************************')  # Remote database URL
+db = client['************'] # Remote database's database name
+collection = db['*********'] # Collection name
+
+@app.route('/store', methods=['POST'])
+#Function to store data
+def store():
+
+    data = request.get_json()
+
+    collection.insert_one(data)
+
+    return jsonify({
+        "message": "Success"
+    })
+
+@app.route('/obtain', methods=['GET'])
+#Function to get data
+def obtain():
+
+    data = list(collection.find())
+
+    for item in data:
+        item['_id'] = str(item['_id'])
+
+    return jsonify(data)
+
+# Start app
+if __name__ == '__main__':
+    WSGIRequestHandler.protocol_version = "HTTP/1.1"
+    app.run(host='0.0.0.0', debug=False, use_reloader=False)
